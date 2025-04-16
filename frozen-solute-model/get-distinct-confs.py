35874163ef33170792e1a3f06c0d7b6859562b71
Michael Wang <michael_wzq@icloud.com>
2025-04-16 11:31:58 +1000

    frozen-solute-model/get-distinct-confs.py

--- frozen-solute-model/get-distinct-confs.py
+++ frozen-solute-model/get-distinct-confs.py
@@ -27,37 +27,79 @@ for compound_id, smiles, iupac, experimental, _, mobley, _, _, _, _, _, _ in mol
         f"SELECT local_path FROM runs WHERE {compound_id = } AND run_type = 'CENSO' LIMIT 1"
     ).fetchone()[0]
 
-    censo_conformer = subprocess.run(["grep", "CONF", f"data/{censo_path}/2_OPTIMIZATION.xyz"], check=True, capture_output=True).stdout.decode('utf-8').split()[0]
+    censo_conformer = (
+        subprocess.run(
+            ["grep", "CONF", f"data/{censo_path}/2_OPTIMIZATION.xyz"],
+            check=True,
+            capture_output=True,
+        )
+        .stdout.decode("utf-8")
+        .split()[0]
+    )
     assert censo_conformer.startswith("CONF"), censo_conformer
 
     vacuum_censo_path = cur.execute(
         f"SELECT local_path FROM runs WHERE {compound_id = } AND run_type = 'VacuumCENSO' LIMIT 1"
     ).fetchone()[0]
 
-    vacuum_censo_conformer = subprocess.run(["grep", "CONF", f"data/{vacuum_censo_path}/2_OPTIMIZATION.xyz"], check=True, capture_output=True).stdout.decode('utf-8').split()[0]
+    vacuum_censo_conformer = (
+        subprocess.run(
+            ["grep", "CONF", f"data/{vacuum_censo_path}/2_OPTIMIZATION.xyz"],
+            check=True,
+            capture_output=True,
+        )
+        .stdout.decode("utf-8")
+        .split()[0]
+    )
     assert vacuum_censo_conformer.startswith("CONF"), vacuum_censo_conformer
 
     if censo_conformer != vacuum_censo_conformer:
         print(compound_id, censo_conformer, vacuum_censo_conformer, smiles, iupac)
         os.makedirs(f"verify/{compound_id}/", exist_ok=True)
 
-        subprocess.run(['cp', f"data/{censo_path}/2_OPTIMIZATION.xyz", f"verify/{compound_id}/censo_input.xyz"], check=True)
+        subprocess.run(
+            [
+                "cp",
+                f"data/{censo_path}/2_OPTIMIZATION.xyz",
+                f"verify/{compound_id}/censo_input.xyz",
+            ],
+            check=True,
+        )
 
-        subprocess.run(['python', 'trim_xyz.py', f"verify/{compound_id}/censo_input.xyz"], check=True)
+        subprocess.run(
+            ["python", "trim_xyz.py", f"verify/{compound_id}/censo_input.xyz"],
+            check=True,
+        )
 
-        subprocess.run(['cp', f"data/{vacuum_censo_path}/2_OPTIMIZATION.xyz", f"verify/{compound_id}/vacuum_censo_input.xyz"], check=True)
+        subprocess.run(
+            [
+                "cp",
+                f"data/{vacuum_censo_path}/2_OPTIMIZATION.xyz",
+                f"verify/{compound_id}/vacuum_censo_input.xyz",
+            ],
+            check=True,
+        )
 
-        subprocess.run(['python', 'trim_xyz.py', f"verify/{compound_id}/vacuum_censo_input.xyz"], check=True)
-
+        subprocess.run(
+            ["python", "trim_xyz.py", f"verify/{compound_id}/vacuum_censo_input.xyz"],
+            check=True,
+        )
 
         basis = "DEF2-TZVP"
 
         with open(f"verify/{compound_id}/censo.inp", "w") as f:
-            f.write(orca_inp_template.format(basis=basis, solvation="SMD(WATER)", input="censo_input.xyz"))
+            f.write(
+                orca_inp_template.format(
+                    basis=basis, solvation="SMD(WATER)", input="censo_input.xyz"
+                )
+            )
 
         with open(f"verify/{compound_id}/vacuum_censo.inp", "w") as f:
-            f.write(orca_inp_template.format(basis=basis, solvation='', input="vacuum_censo_input.xyz"))
+            f.write(
+                orca_inp_template.format(
+                    basis=basis, solvation="", input="vacuum_censo_input.xyz"
+                )
+            )
 
         with open(f"verify/{compound_id}/1", "w") as f:
             f.write(katana_orca_template.format())
-
