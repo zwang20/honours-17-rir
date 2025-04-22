372dfd8776ca46a464f7cd0e3ac300fcc2988188
Michael Wang <michael_wzq@icloud.com>
2025-04-22 11:20:25 +1000

    frozen-solute-model/prep.frozen.py

--- frozen-solute-model/prep.frozen.py
+++ frozen-solute-model/prep.frozen.py
@@ -1,7 +1,11 @@
 # pylint: disable=C0103
 
+# usage: prep.frozen.py <compound_id> <local_path> <remote_host>
+
+import collections
 import sys
 import os
+import sqlite3
 import subprocess
 
 from common import tleap_template
@@ -12,36 +16,115 @@ from common import katana_min_equil, gadi_min_equil
 from common import constraint_frozen
 
 assert len(sys.argv) == 4, (sys.argv, len(sys.argv))
-mobley_id = sys.argv[1]
-assert mobley_id.startswith("mobley_")
+compound_id = sys.argv[1]
+assert compound_id.startswith("mobley_"), compound_id
 local_path = sys.argv[2]
-assert int(local_path) >= 0
+assert int(local_path) >= 0, local_path
 hostname = sys.argv[3]
-assert hostname in ("katana", "katana2", "gadi")
+assert hostname in ("katana", "katana2", "gadi"), hostname
+
+# database
+con = sqlite3.connect("frozen_solute_model_new.db")
+cur = con.cursor()
+
+xyz_path = cur.execute(
+    f"SELECT local_path FROM runs WHERE {compound_id = } AND run_type = 'CENSO' LIMIT 1"
+).fetchone()[0]
+
+retry = 0
+while True:
+    print("mol2")
+    try:
+        subprocess.run(
+            [
+                f"stdbuf -oL xvfb-run iqmol FreeSolv/mol2files_sybyl/{compound_id}.mol2 He.xyz > mol2",
+            ],
+            timeout=15,
+            capture_output=True,
+            shell=True,
+            check=False,
+        )
+    except subprocess.TimeoutExpired:
+        pass
+    d = collections.defaultdict(dict)
+    with open("mol2") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            line = line.strip()
+            if i < 4:
+                continue
+            if not line:
+                break
+            k, v = line.split(maxsplit=1)
+            d[k] = v
+
+    try:
+        assert d
+        break
+    except AssertionError:
+        retry += 1
+        if retry >= 10:
+            print(f"Failing job {compound_id}")
+            cur.execute(
+                f"INSERT INTO runs VALUES ('{compound_id}', 'FrozenMinEquilCENSO3', 'Failed', {local_path}, '{HOSTNAME}', '{REMOTE_PATH}');"
+            )
+            con.commit()
+            con.close()
+            sys.exit(1)
+
+        continue
+
+try:
+    subprocess.run(
+        [
+            f"xvfb-run iqmol data/{xyz_path}/2_OPTIMIZATION.xyz He.xyz > xyz",
+        ],
+        timeout=15,
+        capture_output=True,
+        shell=True,
+        check=False,
+    )
+except subprocess.TimeoutExpired:
+    pass
+
+with open("xyz") as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if i < 4:
+            continue
+        if not line:
+            break
+        k, v = line.split(maxsplit=1)
+        assert d[k] == v
+
+subprocess.run(["killall", "iqmol"], check=False)
+
+subprocess.run(["cp", f"data/{xyz_path}/2_OPTIMIZATION.xyz", "."], check=True)
 
 # copy files
-subprocess.run(["cp", f"FreeSolv/mol2files_gaff/{mobley_id}.mol2", "."], check=True)
-subprocess.run(["chmod", "-x", f"{mobley_id}.mol2"], check=True)
-subprocess.run(["cp", f"FreeSolv/amber/{mobley_id}.frcmod", "."], check=True)
+subprocess.run(["cp", f"FreeSolv/mol2files_gaff/{compound_id}.mol2", "."], check=True)
+subprocess.run(["chmod", "-x", f"{compound_id}.mol2"], check=True)
+subprocess.run(["cp", f"FreeSolv/amber/{compound_id}.frcmod", "."], check=True)
 
 # update coords
 subprocess.run(
     [
         "python",
         "update-mol2-with-xyz.py",
-        f"{mobley_id}.mol2",
+        f"{compound_id}.mol2",
         f"2_OPTIMIZATION.xyz",
-        f"{mobley_id}.new.mol2",
+        f"{compound_id}.new.mol2",
     ],
     check=True,
 )
-subprocess.run(["mv", f"{mobley_id}.new.mol2", f"{mobley_id}.mol2"], check=True)
+subprocess.run(["mv", f"{compound_id}.new.mol2", f"{compound_id}.mol2"], check=True)
 
 low, high = (0.0, 20.0)
 curr = 10.0
 while True:
     with open("leap.in", "w", encoding="utf-8") as f:
-        f.write(tleap_template.format(mobley_id=mobley_id, size=curr))
+        f.write(tleap_template.format(mobley_id=compound_id, size=curr))
 
     # solvate molecule
     subprocess.run(
@@ -56,19 +139,19 @@ while True:
             "sed",
             "-i",
             "/MOL/s/1\\.00  0\\.00/1.00  1.00/g",
-            f"{mobley_id}.pdb",
+            f"{compound_id}.pdb",
         ],
         check=True,
     )
 
     # get size
-    with open(f"{mobley_id}.pdb", encoding="utf-8") as f:
+    with open(f"{compound_id}.pdb", encoding="utf-8") as f:
         x, y, z = map(float, f.readline().split(maxsplit=4)[1:4])
         print(x, y, z)
 
     num_atoms = int(
         subprocess.run(
-            [f"cat {mobley_id}.pdb | grep ATOM | wc -l"],
+            [f"cat {compound_id}.pdb | grep ATOM | wc -l"],
             shell=True,
             capture_output=True,
             check=True,
@@ -90,41 +173,41 @@ while True:
 os.makedirs(f"data/{local_path}", exist_ok=True)
 
 # move files
-subprocess.run(["mv", f"{mobley_id}.inpcrd", f"data/{local_path}/"], check=True)
-subprocess.run(["mv", f"{mobley_id}.prmtop", f"data/{local_path}/"], check=True)
-subprocess.run(["mv", f"{mobley_id}.pdb", f"data/{local_path}/"], check=True)
+subprocess.run(["mv", f"{compound_id}.inpcrd", f"data/{local_path}/"], check=True)
+subprocess.run(["mv", f"{compound_id}.prmtop", f"data/{local_path}/"], check=True)
+subprocess.run(["mv", f"{compound_id}.pdb", f"data/{local_path}/"], check=True)
 
 # clean up files
-subprocess.run(["rm", f"{mobley_id}.frcmod"], check=True)
-subprocess.run(["rm", f"{mobley_id}.mol2"], check=True)
+subprocess.run(["rm", f"{compound_id}.frcmod"], check=True)
+subprocess.run(["rm", f"{compound_id}.mol2"], check=True)
 subprocess.run(["rm", f"leap.log"], check=True)
 
 # write input files
 with open(f"data/{local_path}/min.namd", "w", encoding="utf-8") as f:
     f.write(
         generic_template.format(
-            io=min_io.format(mobley_id=mobley_id),
+            io=min_io.format(mobley_id=compound_id),
             x=x,
             y=y,
             z=z,
             run=min_run,
-            mobley_id=mobley_id,
+            mobley_id=compound_id,
             margin=8.0,
-            constraints=constraint_frozen.format(mobley_id=mobley_id),
+            constraints=constraint_frozen.format(mobley_id=compound_id),
         )
     )
 
 with open(f"data/{local_path}/equil.namd", "w", encoding="utf-8") as f:
     f.write(
         generic_template.format(
-            io=equil_io.format(mobley_id=mobley_id),
+            io=equil_io.format(mobley_id=compound_id),
             run=equil_run,
             x=x,
             y=y,
             z=z,
-            mobley_id=mobley_id,
+            mobley_id=compound_id,
             margin=8.0,
-            constraints=constraint_frozen.format(mobley_id=mobley_id),
+            constraints=constraint_frozen.format(mobley_id=compound_id),
         )
     )
 
